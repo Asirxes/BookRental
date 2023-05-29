851dc0f3a4490c1ee8733667cfe53a691815067c
AsiaK21 <asiak21@onet.eu>
2023-05-29 14:52:06 +0200

    ciasteczko

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginPageComponent } from './login-page/login-page.component';
-import {MatIconModule} from '@angular/material/icon'
+import {MatIconModule} from '@angular/material/icon';
+import { SecondpageComponent } from './secondpage/secondpage.component'
 @NgModule({
   declarations: [
     AppComponent,
-    LoginPageComponent
+    LoginPageComponent,
+    SecondpageComponent
   ],
   imports: [
     BrowserModule,
