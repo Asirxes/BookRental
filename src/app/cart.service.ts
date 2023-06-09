1d3b4ead566e8c0f8a4d4525346e4b36618b1d9b
AsiaK21 <asiak21@onet.eu>
2023-06-09 17:47:55 +0200

    koszyk

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,31 +1,85 @@
- import { Injectable } from '@angular/core';
- import { Book } from './book';
+//  import { Injectable } from '@angular/core';
+//  import { Book } from './book';
 
- @Injectable({
-   providedIn: 'root'
- })
- export class CartService {
-   cartItems: Book[] = [];
+//  @Injectable({
+//    providedIn: 'root'
+//  })
+//  export class CartService {
+//    cartItems: Book[] = [];
 
-   constructor() { }
+//    constructor() { }
 
-   addToCart(book: Book) {
-     this.cartItems.push(book);
-   }
+//    addToCart(book: Book) {
+//      this.cartItems.push(book);
+//    }
 
-   removeCartItem(book: Book) {
-     const index = this.cartItems.indexOf(book);
-     if (index > -1) {
-       this.cartItems.splice(index, 1);
-     }
-   }
+//    removeCartItem(book: Book) {
+//      const index = this.cartItems.indexOf(book);
+//      if (index > -1) {
+//        this.cartItems.splice(index, 1);
+//      }
+//    }
 
-   getCartItems() {
-     return this.cartItems;
-   }
+//    getCartItems() {
+//      return this.cartItems;
+//    }
 
-   clearCart() {
-     this.cartItems = [];
-   }
- }
+//    clearCart() {
+//      this.cartItems = [];
+//    }
+//  }
 
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CartService {
+  cartItems: any[] = [{
+    title: 'Duma i uprzedzenie',
+    author: 'Jane Austen',
+    price: 35.99,
+    quantity: 1,
+  },
+  {
+    title: 'Harry Potter',
+    author: 'Inny autor',
+    price: 29.99,
+    quantity: 1,
+  },
+  {
+    title: 'Inna książka',
+    author: 'Jeszcze inny autor',
+    price: 20.99,
+    quantity: 1,
+  },];
+
+  addToCart(book: any) {
+    this.cartItems.push(book);
+  }
+
+  removeFromCart(item: any) {
+    const index = this.cartItems.indexOf(item);
+    if (index > -1) {
+      this.cartItems.splice(index, 1);
+    }
+  }
+
+  updateQuantity(item: any) {
+    if (item.quantity < 1) {
+      item.quantity = 1;
+    }
+  }
+
+  getCartItems(): any[] {
+    return this.cartItems;
+  }
+
+  calculateTotalPrice(): number {
+    let total = 0;
+    for (const item of this.cartItems) {
+      total += item.price * item.quantity;
+    }
+    return total;
+  }
+}
--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -37,39 +37,80 @@
 //     alert('Płatność została sfinalizowana. Dziękujemy za zakupy!');
 //   }
 // }
+
+
+
+// import { Component } from '@angular/core';
+// import { CartService } from '../cart.service';
+// import { Book } from '../book';
+// interface CartItem {
+//   book: Book;
+//   quantity: number;
+// }
+
+// @Component({
+//   selector: 'app-cart',
+//   templateUrl: './cart.component.html',
+//   styleUrls: ['./cart.component.css']
+// })
+// export class CartComponent {
+//   cartItems: CartItem[] = [];
+
+//   addToCart(book: Book) {
+//     const existingCartItem = this.cartItems.find(item => item.book === book);
+//     if (existingCartItem) {
+//       existingCartItem.quantity++;
+//     } else {
+//       this.cartItems.push({ book, quantity: 1 });
+//     }
+//   }
+
+//   removeFromCart(cartItem: CartItem) {
+//     const index = this.cartItems.indexOf(cartItem);
+//     if (index !== -1) {
+//       this.cartItems.splice(index, 1);
+//     }
+//   }
+
+//   updateQuantity(cartItem: CartItem, newQuantity: number) {
+//     cartItem.quantity = newQuantity;
+//   }
+// }
+
 import { Component } from '@angular/core';
 import { CartService } from '../cart.service';
-import { Book } from '../book';
-interface CartItem {
-  book: Book;
-  quantity: number;
-}
 
 @Component({
-  selector: 'app-cart',
-  templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
-})
+     selector: 'app-cart',
+     templateUrl: './cart.component.html',
+     styleUrls: ['./cart.component.css']
+   })
+
 export class CartComponent {
-  cartItems: CartItem[] = [];
+  cartItems: any[] = [];
 
-  addToCart(book: Book) {
-    const existingCartItem = this.cartItems.find(item => item.book === book);
-    if (existingCartItem) {
-      existingCartItem.quantity++;
-    } else {
-      this.cartItems.push({ book, quantity: 1 });
-    }
+  constructor(private cartService: CartService) {
+    this.cartItems = this.cartService.getCartItems(); // dorzucenie ksiązek na sztywno
   }
 
-  removeFromCart(cartItem: CartItem) {
-    const index = this.cartItems.indexOf(cartItem);
-    if (index !== -1) {
-      this.cartItems.splice(index, 1);
-    }
+  // ngOnInit() {
+  //   this.cartItems = this.cartService.getCartItems(); //wcześniejszy dorzut książek
+  // }
+
+  removeFromCart(item: any) {
+    this.cartService.removeFromCart(item);
   }
 
-  updateQuantity(cartItem: CartItem, newQuantity: number) {
-    cartItem.quantity = newQuantity;
+  updateQuantity(item: any) {
+    this.cartService.updateQuantity(item);
+  }
+
+  calculateTotalPrice(): number {
+    let total = 0;
+    for (const item of this.cartItems) {
+      total += item.price * item.quantity;
+    }
+    return total;
   }
 }
+
