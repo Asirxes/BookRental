2bb0f7ceeb01d8319d00f3448f1e6781ad894206
AsiaK21 <asiak21@onet.eu>
2023-06-14 19:30:48 +0200

    ?

--- frontend/src/app/charts/charts.component.ts
+++ frontend/src/app/charts/charts.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import Chart from 'chart.js/dist/core/core.controller';
+// import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+// import Chart from 'chart.js/dist/core/core.controller';
+import {Component, Input, OnInit} from '@angular/core';
+import {ChartConfiguration, ChartData, ChartType} from "chart.js";
+import DataLabelsPlugin from "chartjs-plugin-datalabels";
 
 @Component({
   selector: 'app-charts',
@@ -8,54 +11,31 @@ import Chart from 'chart.js/dist/core/core.controller';
   styleUrls: ['./charts.component.css']
 })
 export class ChartsComponent implements OnInit {
-  @ViewChild('myChart') myChartRef!: ElementRef;
-  chartData: any;
-
-  constructor(private http: HttpClient) { }
-
-  ngOnInit() {
-    this.fetchChartData();
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
   }
+  // @ViewChild('myChart') myChartRef!: ElementRef;
+  // chartData: any;
 
-  fetchChartData() {
-    this.http.get('API_URL').subscribe((data: any) => {
-      // Przetwarzanie danych otrzymanych z API
-      this.chartData = data;
-      this.generateChart();
-    });
-  }
+  // constructor(private http: HttpClient) { }
 
-  generateChart() {
-    const labels = this.chartData.labels;
-    const data = this.chartData.data;
+  // ngOnInit() {
+  //   this.fetchChartData();
+  // }
 
-    // Wygenerowanie wykresu
-    // Możesz skorzystać z biblioteki Chart.js lub innej biblioteki do generowania wykresów
+  // fetchChartData() {
+  //   this.http.get('API_URL').subscribe((data: any) => {
+  //     this.chartData = data;
+  //     this.generateChart();
+  //   });
+  // }
 
-    // Przykładowe użycie biblioteki Chart.js
-    const canvas = this.myChartRef.nativeElement as HTMLCanvasElement;
-    const ctx = canvas.getContext('2d');
+  @Input() public barChartOptions: ChartConfiguration['options'];
+  @Input() public barChartData: ChartData<'bar'> = {
+      labels: [],
+      datasets: []
+  };
 
-    new Chart(ctx, {
-      type: 'bar',
-      data: {
-        labels: labels,
-        datasets: [{
-          label: 'My Chart',
-          data: data,
-          backgroundColor: 'rgba(0, 123, 255, 0.5)',
-          borderColor: 'rgba(0, 123, 255, 1)',
-          borderWidth: 1
-        }]
-      },
-      options: {
-        responsive: true,
-        scales: {
-          y: {
-            beginAtZero: true
-          }
-        }
-      }
-    });
-  }
+  protected barChartType: ChartType = 'bar';
+  protected barChartPlugins = [DataLabelsPlugin];
 }
