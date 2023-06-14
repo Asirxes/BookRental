b292909d25880fca1b017c6d92c8c433ace8fc70
AsiaK21 <asiak21@onet.eu>
2023-06-14 19:49:30 +0200

    y

--- frontend/src/app/charts/charts.component.ts
+++ frontend/src/app/charts/charts.component.ts
@@ -1,6 +1,3 @@
-// import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-// import { HttpClient } from '@angular/common/http';
-// import Chart from 'chart.js/dist/core/core.controller';
 import {Component, Input, OnInit} from '@angular/core';
 import {ChartConfiguration, ChartData, ChartType} from "chart.js";
 import DataLabelsPlugin from "chartjs-plugin-datalabels";
@@ -10,25 +7,7 @@ import DataLabelsPlugin from "chartjs-plugin-datalabels";
   templateUrl: './charts.component.html',
   styleUrls: ['./charts.component.css']
 })
-export class ChartsComponent implements OnInit {
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
-  }
-  // @ViewChild('myChart') myChartRef!: ElementRef;
-  // chartData: any;
-
-  // constructor(private http: HttpClient) { }
-
-  // ngOnInit() {
-  //   this.fetchChartData();
-  // }
-
-  // fetchChartData() {
-  //   this.http.get('API_URL').subscribe((data: any) => {
-  //     this.chartData = data;
-  //     this.generateChart();
-  //   });
-  // }
+export class ChartsComponent {
 
   @Input() public barChartOptions: ChartConfiguration['options'];
   @Input() public barChartData: ChartData<'bar'> = {
